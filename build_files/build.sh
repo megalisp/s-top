3bbf8e3685556a4c9d0f4c3f52aceacbb9e2c7a8
( ) MEGA [ie: Jost Grant] <source.megalisp@proton.me>
2025-08-05 18:51:14 -0500

    Move revelant packages to RackOS.

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,50 +2,17 @@
 
 set -ouex pipefail
 
-### Install packages
+
 set -ouex pipefail && \
   dnf5 install -y \
-    openbox greetd tuigreet gtkgreet \
+    openbox greetd tuigreet gtkgreet arandr \
     xdg-utils xdg-user-dirs \
     xorg-x11-server-Xorg xorg-x11-xinit \
     xorg-x11-drv-evdev xorg-x11-drv-libinput xorg-x11-drv-vesa xorg-x11-drv-vmware \
     xsecurelock xdotool xload xclip xclock xkill xterm xset xsetroot xmodmap xrandr \
-    papirus-icon-theme google-noto-emoji-fonts google-nono-fonts-all dejavu-sans-fonts \
-    google-roboto-fonts google-roboto-mono-fonts google-roboto-slab-fonts \
-    gnome-keyring pipewire pipewire-pulseaudio wireplumber pavucontrol \
-    qemu-kvm x11vnc obs-studio vlc ffmpeg sox \
-    chatterino2 v4l-utils v4l2loopback \
-    picom arandr thunar tumbler \
-    mpv mpd playerctl rofi feh \
+    pipewire pipewire-pulseaudio wireplumber \
     network-manager-applet \
-    input-leap kde-connect \
-    python3-streamlink yt-dlp \
-    cef chromium \
-    chez-scheme \
-    chibi-scheme \
     --skip-unavailable
-
-
-dnf5 install -y git dnf5-plugins
-
-export DNF5_ASSUME_ALWAYS_YES=true
-export DNF5_DISABLE_REPO_EXCLUDES=all
-
-dnf5 config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo
-dnf5 install -y gh --repo gh-cli
-
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-tee /etc/yum.repos.d/vscode.repo > /dev/null <<EOF
-[code]
-name=Visual Studio Code
-baseurl=https://packages.microsoft.com/yumrepos/vscode
-enabled=1
-autorefresh=1
-type=rpm-md
-gpgcheck=1
-gpgkey=https://packages.microsoft.com/keys/microsoft.asc
-EOF
-dnf5 install -y code --repo code
 
 dnf5 clean all
 
